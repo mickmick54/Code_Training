f032aee0b5cd0060ad77bbe62b744a8e48755f85
Mickaël Colleu <6249403+mickmick54@users.noreply.github.com>
2023-01-20 16:08:22 +0100

    save exo 4
    exo 4

--- java/j_01_matheux/exo_3/src/App.java
+++ java/j_01_matheux/exo_3/src/App.java
@@ -4,9 +4,20 @@ public class App {
     /**
      * @param args
      * @throws Exception
+     * 
+     * Variables a, b, c en Caratères
+     * Début
+     * Ecrire "Ecrivez successivement trois noms :"
+     * Lire a, b, c
+        * Si a < b ET b < c Alors
+        *      Ecrire "Ces noms sont classés alphabétiquement"
+        * Sinon
+        *      Ecrire "Ces noms ne sont pas classés"
+        * Finsi
+     * Fin
      */
     public static void main(String[] args) throws Exception {
-        
+
         String a, b, c;
 
         Scanner sc = new Scanner(System.in);
@@ -17,7 +28,11 @@ public class App {
         b = sc.nextLine();
         c = sc.nextLine();
 
-        if ((a < b) && (b < c)) {
+        int number1 = Integer.parseInt(a);
+        int number2 = Integer.parseInt(b);
+        int number3 = Integer.parseInt(c);
+
+        if ((number1 < number2) && (number2 < number3)) {
 
             System.out.println("Ces noms sont classés alphabétiquement");
         } else {
